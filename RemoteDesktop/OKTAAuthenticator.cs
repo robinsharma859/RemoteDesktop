d0dc9ba622ab8e153ce94a1ecd644c42e55acefc
Rahul Sharma <Rahul.Sharma@winshuttle.com>
2020-11-12 00:00:36 +0530

    adding dll

---
+++ RemoteDesktop/OKTAAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OtpNet;
+
+
+namespace RemoteDesktop
+{
+    public class OKTAAuthenticator
+    {
+      private  Totp otpReader;
+        public OKTAAuthenticator()
+        {
+            byte[] secreatKey = Encoding.ASCII.GetBytes(("080693"));
+            otpReader = new Totp(secreatKey);
+            otpReader.ComputeTotp(DateTime.UtcNow);
+        }
+    }
+}
