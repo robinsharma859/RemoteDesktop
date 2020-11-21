ef700ea784a6856bf45d3a5d72959b1b518dd96d
Rahul Sharma <Rahul.Sharma@winshuttle.com>
2020-11-21 23:51:54 +0530

    adding new files

---
+++ GlobalParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public  class GlobalParameters
+{
+    public static string DeploymentMode { get; set; }
+    public static string Retry { get; set; }
+    public static string RunBeforeSuite { get; set; }
+    public static string URL { get; set; }
+    public static string UserName { get; set; }
+    public static string Browser { get; set; }
+}
---
+++ RemoteDesktop/SetGlobalParameters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RemoteDesktop
+{
+    sealed class SetGlobalParameters
+    {
+        private XmlDocument xmlDocument = null;
+        string globalPath = new DirectoryInfo(Assembly.GetExecutingAssembly().Location.ToString()).Parent.Parent.Parent.FullName.ToString() + "\\Config\\Config.xml";
+        public SetGlobalParameters()
+        {
+            xmlDocument = new XmlDocument();
+            xmlDocument.Load(globalPath);
+        }
+
+        public void SetGlobalParams()
+        {
+            Console.WriteLine("Setting Global Parameters");
+            GlobalParameters.URL = xmlDocument["Configuration"]["URL"].InnerText;
+            Console.WriteLine("URL is " +  GlobalParameters.URL);
+            GlobalParameters.UserName = xmlDocument["Configuration"]["UserName"].InnerText.Replace("{",string.Empty).Replace("}",string.Empty);
+            Console.WriteLine("UserName is " + GlobalParameters.UserName);
+
+            GlobalParameters.Browser = xmlDocument["Configuration"]["Browser"].InnerText.Replace("{", string.Empty).Replace("}", string.Empty);
+            Console.WriteLine("Browser is " + GlobalParameters.Browser);
+
+        }
+    }
+}
